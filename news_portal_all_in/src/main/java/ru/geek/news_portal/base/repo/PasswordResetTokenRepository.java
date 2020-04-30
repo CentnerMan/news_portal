c70d96de956b37303709cca83400c6977fdc16f6
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2020-04-30 21:02:54 +0300

    Восстановление пароля по почте (v 1.0)

---
+++ news_portal_all_in/src/main/java/ru/geek/news_portal/base/repo/PasswordResetTokenRepository.java
@@ -0,0 +1,12 @@
+package ru.geek.news_portal.base.repo;
+
+/**
+ * GeekBrains Java, news_portal.
+ *
+ * @author Anatoly Lebedev
+ * @version 1.0.0 30.04.2020
+ * @link https://github.com/Centnerman
+ */
+
+public class PasswordResetTokenRepository {
+}
---
+++ news_portal_all_in/src/main/java/ru/geek/news_portal/services/SecurityService.java
@@ -0,0 +1,12 @@
+package ru.geek.news_portal.services;
+
+/**
+ * GeekBrains Java, news_portal.
+ *
+ * @author Anatoly Lebedev
+ * @version 1.0.0 30.04.2020
+ * @link https://github.com/Centnerman
+ */
+
+public class SecurityService {
+}
